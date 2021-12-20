3aa31605b805341d98496e47261fbcbc37577b43
medineannamammedova <medineannamammedova@Medine-MBP.fritz.box>
2021-12-20 23:08:11 +0100

    first

---
+++ src/karisik/C01_AlfabeYazdir.java
@@ -0,0 +1,28 @@
+package karisik;
+
+public class C01_AlfabeYazdir {
+
+	public static void main(String[] args) {
+		
+			/*
+			 * Konsolda Alfabeyi Yazd�ran programi yaziniz.
+			 * 
+			 * OUTPUT :
+			 * 
+			 * a b c .. .. .. .. y z
+			 */
+
+			
+			for (char i = 'a'; i <='z'; i++) {
+				System.out.print(i+" ");
+			}
+			
+			
+			
+			
+			
+	
+	
+	}
+
+}
